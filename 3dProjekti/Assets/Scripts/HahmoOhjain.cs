7924e912a9fd92d146c7181bc1a2e96059138c40
unknown <kartila2005@gmail.com>
2021-05-04 18:29:32 +0300

    hyppy ja katseleminen kuntoon

--- 3dProjekti/Assets/Scripts/HahmoOhjain.cs
+++ 3dProjekti/Assets/Scripts/HahmoOhjain.cs
@@ -8,21 +8,38 @@ public class HahmoOhjain : MonoBehaviour
     public float hiirenNopeus = 3f;
     public float hyppyNopeus = 100f;
     public float painovoima = 10f;
+    public float maxKaannosAsteet = 60;
+    public float minKaannosAsteet = -70;
+    public CursorLockMode haluttuMoodi;
     private float vertikaalinnenPyorinta = 0;
     private float horisontaalinenPyorinta = 0;
     private Vector3 liikesuunta = Vector3.zero;
+    private CharacterController controller;
     // Start is called before the first frame update
     void Start()
     {
-        
+       Cursor.lockState = haluttuMoodi;
+       Cursor.visible = (CursorLockMode.Locked != haluttuMoodi);
+       controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
         horisontaalinenPyorinta += Input.GetAxis("Mouse X") * hiirenNopeus;
-        vertikaalinnenPyorinta += Input.GetAxis("Mouse Y") * hiirenNopeus;
+        vertikaalinnenPyorinta -= Input.GetAxis("Mouse Y") * hiirenNopeus;
+        vertikaalinnenPyorinta = Mathf.Clamp(vertikaalinnenPyorinta, minKaannosAsteet, maxKaannosAsteet);
         Camera.main.transform.localRotation = Quaternion.Euler(vertikaalinnenPyorinta,horisontaalinenPyorinta,0);
-
+        float nopeusEteen = Input.GetAxis("Vertical");
+        float nopeusSivulle = Input.GetAxis("Horizontal");
+        Vector3 nopeus = new Vector3(nopeusSivulle * juoksuNopeus,0,nopeusEteen * juoksuNopeus);
+        nopeus = transform.rotation * nopeus;
+        controller.SimpleMove(nopeus);
+        liikesuunta.y -= painovoima * Time.deltaTime;
+        controller.Move(liikesuunta * Time.deltaTime);
+        if(controller.isGrounded && Input.GetButtonDown("Jump"))
+        {
+            liikesuunta.y += hyppyNopeus;
+        }
     }
 }
